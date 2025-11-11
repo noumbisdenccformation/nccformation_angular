bc59f9c6353c9aae00fa2f09f9bf19de3667430a
noumbisdenccformation <noumbissiej@proton.me>
2025-11-11 06:08:51 +0100

    feat: redirection bouton Connexion vers formulaire amélioré

--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -15,31 +15,4 @@ export class HeaderComponent {
   navigateTo(route: string) {
     this.router.navigate([route]);
   }
-
-  // NOUVELLES propriétés pour le modal
-  isAuthModalOpen = false;
-  activeTab: 'login' | 'register' = 'login';
-
-  // Méthodes existantes (navigateTo, etc.)...
-
-  // NOUVELLES méthodes
-  openAuthModal() {
-    this.isAuthModalOpen = true;
-  }
-
-  closeAuthModal() {
-    this.isAuthModalOpen = false;
-  }
-
-  onLogin() {
-    // TODO: Implémenter la logique de connexion
-    console.log('Connexion en cours...');
-    this.closeAuthModal();
-  }
-
-  onRegister() {
-    // TODO: Implémenter la logique d'inscription
-    console.log('Inscription en cours...');
-    this.closeAuthModal();
-  }
 }
